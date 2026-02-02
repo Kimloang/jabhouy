ebe5a3ce2661648a4edf0ac76a03525cb4c3902b
Kimloang <94738831+Kimloang@users.noreply.github.com>
2026-02-02 11:47:58 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,8 +5,8 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: './', // Use './' for relative paths or '/repo-name/' for GitHub Pages
-  base: 'jabhouy' // Use './' for relative paths or '/repo-name/' for GitHub Pages
+  base: './', // Use './' for relative paths or '/repo-name/' for GitHub Pages
+  // base: 'jabhouy' // Use './' for relative paths or '/repo-name/' for GitHub Pages
   // build: {
   //   outDir: 'dist',
   //   sourcemap: false,
